a3acbcd2ad1bc5ee983b2340e2e3f9c22545cbed
KhaoYang <kevinhyang@outlook.com>
2025-08-25 21:23:16 -0500

    setting up app design

---
+++ MapRUNNER/src/App.jsx
@@ -0,0 +1,16 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div id = 'map-container'/>
+    </>
+  )
+}
+
+export default App
