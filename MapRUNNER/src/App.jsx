356fdf5e564b3853114135620fd2335492997b0e
KhaoYang <kevinhyang@outlook.com>
2025-08-25 21:45:06 -0500

    implement map visualization

--- MapRUNNER/src/App.jsx
+++ MapRUNNER/src/App.jsx
@@ -6,9 +6,22 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import './App.css'
 
 function App() {
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2hhb3lhbmciLCJhIjoiY21lcnhld2N3MGMwcjJpcTZtNWxqNHAxYiJ9.mDZuEmUsH2Z16QPAhDRcBg'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+  });
+    return () => {
+      mapRef.current.remove()
+    }
+  },[])
+  
   return (
     <>
-      <div id = 'map-container'/>
+      <div id = 'map-container' ref = {mapContainerRef}/>
     </>
   )
 }
