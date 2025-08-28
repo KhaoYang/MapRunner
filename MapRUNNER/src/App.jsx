d4a9b839fa0bf036d3808d8bcb288f8f87b4de32
KhaoYang <kevinhyang@outlook.com>
2025-08-27 23:34:28 -0500

    added sidebar and initial location set

--- MapRUNNER/src/App.jsx
+++ MapRUNNER/src/App.jsx
@@ -1,28 +1,48 @@
-
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
-import 'mapbox-gl/dist/mapbox-gl.css'
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
+
+const INITIAL_CENTER = [
+  -88.2294,
+  40.1020
+]
+const INITIAL_ZOOM = 10.12
 
 function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-  });
+      center: center,
+      zoom: zoom
+    });
+
+    mapRef.current.on('move', () => {
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+      setCenter([ mapCenter.lng, mapCenter.lat ])
+      setZoom(mapZoom)
+    })
+
     return () => {
       mapRef.current.remove()
     }
-  },[])
-  
+  }, [])
+
   return (
     <>
-      <div id = 'map-container' ref = {mapContainerRef}/>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <div id='map-container' ref={mapContainerRef} />
     </>
   )
 }
