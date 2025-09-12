94c12f3cd4e4775e9f914f74b40e6df79fdc5d2b
KhaoYang <kevinhyang@outlook.com>
2025-09-12 01:07:09 -0500

    express modifications

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 //import cors from 'cors';
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("testing phase");
+});
 app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000 hello");
+    console.log("Server started at http://localhost:5000");
 });
