269b701d571b63517cb4906a2593e3d0c30b920e
KhaoYang <kevinhyang@outlook.com>
2025-09-12 00:38:15 -0500

    file reorganization pt 2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 const app = express();
 app.listen(5000, () => {
-    console.log("Server started on port 5000");
+    console.log("Server started at http://localhost:5000 hello");
 });
